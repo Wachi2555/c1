bad5972a2382b61587839e5d55e04f16ab48f15a
Wachi2555 <115542583+Wachi2555@users.noreply.github.com>
2022-10-11 16:50:57 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+robotbit.Servo(robotbit.Servos.S1, 180)
+robotbit.Servo(robotbit.Servos.S2, 180)
+robotbit.Servo(robotbit.Servos.S3, 180)
+robotbit.Servo(robotbit.Servos.S4, 180)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
